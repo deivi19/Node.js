7847cad2ff4a85f8f7b563f40070b9bd09bdc2a7
deviperez29 <vidaldeivi7@gmail.com>
2026-04-15 09:27:28 -0500

    se genero un error desde develop

--- src/app.js
+++ src/app.js
@@ -32,12 +32,6 @@ version: "1.0.0"
 }
 
 export function calcularFactorial(n) {
-    if (n < 0) return "Error: No existe el factorial de un número negativo";
-    if (n === 0 || n === 1) return 1;
     
-    let resultado = 1;
-    for (let i = 2; i <= n; i++) {
-        resultado *= i;
-    }
-    return resultado;
+    return null;
 }
