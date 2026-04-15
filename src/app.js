8e2d3dad3deaabdb082108665376339d3611128f
deviperez29 <vidaldeivi7@gmail.com>
2026-04-15 10:01:36 -0500

    Hotflix(api): Se arrego el bug generado por la funcion factorial

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 export function saludar(nombre){
-return "Hola" + nombre + ", bienvenido a aplicacion de Deivi";
+return "Hola" + nombre + ", bienvenido a aplicacion de Ary";
 }
 export function despedir(nombre){
-return "Hasta pronto," + nombre + "!Deivi";
+return "Hasta pronto," + nombre + "!Ary";
 }
 
 export function estadoSistema(){
 return {
 estado: "activo",
 version: "1.0.0",
-message: "Sistema Deivi funcionando correctamente"
+message: "Sistema Ary funcionando correctamente"
 }; 
 }
 
 export function sumar(a ,b){
+
 return  a+b ;
 }
 
@@ -31,6 +32,12 @@ version: "1.0.0"
 }
 
 export function calcularFactorial(n) {
+    if (n < 0) return "Error: No existe el factorial de un número negativo";
+    if (n === 0 || n === 1) return 1;
     
-    return null;
+    let resultado = 1;
+    for (let i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
 }
